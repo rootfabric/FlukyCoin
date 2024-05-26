9ed5ad3d67810c639c2f5d2efec64df77bb70f14
root <yglamazdin@gmail.com>
2024-05-27 01:39:02 +1000

    feat: Тестирование валидаци цепи заготовка

---
+++ tests/chain_test_work.py
@@ -0,0 +1,16 @@
+from storage.chain import Chain
+from core.protocol import Protocol
+from core.Block import Block
+
+if __name__ == '__main__':
+    """ Тестирование валидаци цепи"""
+
+    """ создание цепи """
+
+    """ Добавление блоков """
+
+    """ Создание транзакции и валидация в цепи """
+
+    """ Создание блока с транзакциями и валидация в цепи """
+
+
